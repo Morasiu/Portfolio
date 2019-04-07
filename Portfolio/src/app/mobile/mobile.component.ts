5a66da15bcaff9918edf8e6aa9c0a4ff56c2c4f5
Hubert Morawski <morasiu2@gmail.com>
2019-04-07 17:30:33 +0200

    update

--- Portfolio/src/app/mobile/mobile.component.ts
+++ Portfolio/src/app/mobile/mobile.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mobile',
   templateUrl: './mobile.component.html',
   styleUrls: ['./mobile.component.less']
 })
-export class MobileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class MobileComponent {
+  @Input() name:string;
+  @Input() image_src: string;
+  @Input() short_description: string;
 }
