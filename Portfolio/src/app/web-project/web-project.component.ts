34f1e24cc60e94b28b41aee3d1c847c873aef5a2
Morasiu <moras2601@wp.pl>
2019-06-28 11:43:59 +0200

    Quickfix for web

--- Portfolio/src/app/web-project/web-project.component.ts
+++ Portfolio/src/app/web-project/web-project.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   <div class="container">
     <h1 class="text-light text-center">{{name}}</h1>
     <p class="text-light text-center">{{time}}</p>
-    <pre class="text-light text-center">{{description}}</pre>
+    <p class="text-light text-center">{{description}}</p>
     <div *ngFor="let tech of technologiesList">
       <div class="text-light offset-md-3"><app-skill name="{{tech}}"></app-skill></div>
     </div>
