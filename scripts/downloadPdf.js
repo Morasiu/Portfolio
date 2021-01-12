4577556548d2ada3d2442f5006938c1b28de8c2a
Morasiu <morasiu2@gmail.com>
2021-01-12 18:58:06 +0000

    Deploying to gh-pages from  @ 8d10693f29f213bfdd55de531fa4ac8e7efbd60a 🚀

---
+++ scripts/downloadPdf.js
@@ -0,0 +1,14 @@
+﻿function downloadPdfJs(element) {
+    var element = document.getElementById(element);
+    
+    $('.skill_icon').css('filter', 'invert(0%) sepia(96%) saturate(16%) hue-rotate(282deg) brightness(89%) contrast(104%)');
+
+    element.classList.add("text-dark");
+    for (let el of document.querySelectorAll('.hide_print')) el.style.visibility = 'hidden';
+
+    window.print();
+    
+    element.classList.remove("text-dark"); 
+    $('.hide-print').css("visibility", "visible !important");
+    for (let el of document.querySelectorAll('.hide_print')) el.style.visibility = 'visible';
+}
