dbe891a37ff5f17842861aaad97ace8ba80c75c8
Morasiu <moras2601@wp.pl>
2021-01-16 13:11:14 +0100

    Better timeline and more info

--- Portfolio/Portfolio/Services/EventImageService.cs
+++ Portfolio/Portfolio/Services/EventImageService.cs
@@ -3,8 +3,9 @@
 namespace Portfolio.Services {
 	public class EventImageService {
 		public static Dictionary<string, string> Images { get; } = new() {
-			{"City Coders Płock", "Images/Events/Logo_City_coders_plock.png"},
-			{"Sheep Your Hack", "Images/Events/sheep_your_hack.png"},
+			{"City Coders Płock", "Images/Achievements/Logo_City_coders_plock.png"},
+			{"Sheep Your Hack", "Images/Achievements/sheep_your_hack.png"},
+			{"BulldogJob article", "Images/Achievements/BulldogJob_logo.png"},
 		};
 	}
 }
