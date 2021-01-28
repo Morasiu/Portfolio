0959f72a013494cc8bdcb123533c6778dba4da8a
Morasiu <morasiu2@gmail.com>
2021-01-28 09:30:23 +0000

    Deploying to gh-pages from master @ 3b2195f6c3f6a3b65049ac0c23cfebde0c910f52 🚀

--- scripts/downloadPdf.js
+++ scripts/downloadPdf.js
@@ -5,12 +5,12 @@
 
     element.classList.add("text-dark");
     for (let el of document.querySelectorAll('.hide_print')) el.style.visibility = 'hidden';
+    for (let el of document.querySelectorAll('.show_print')) el.style.visibility = 'visible';
 
     window.print();
     
     element.classList.remove("text-dark"); 
     $('.skill_icon').css('filter', 'invert(100%) sepia(11%) saturate(2%) hue-rotate(329deg) brightness(108%) contrast(101%)');
-
-    $('.hide-print').css("visibility", "visible !important");
     for (let el of document.querySelectorAll('.hide_print')) el.style.visibility = 'visible';
+    for (let el of document.querySelectorAll('.show_print')) el.style.visibility = 'hidden';
 }
