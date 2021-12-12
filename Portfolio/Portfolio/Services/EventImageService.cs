7def6c63b8e336d8745d15a69f7720c3db214e54
Morasiu <moras2601@wp.pl>
2021-12-12 13:11:39 +0100

    SheepYourHack3

--- Portfolio/Portfolio/Services/EventImageService.cs
+++ Portfolio/Portfolio/Services/EventImageService.cs
@@ -5,6 +5,7 @@ namespace Portfolio.Services {
 		public static Dictionary<string, string> Images { get; } = new() {
 			{"City Coders Płock", "Images/Achievements/Logo_City_coders_plock.png"},
 			{"Sheep Your Hack", "Images/Achievements/sheep_your_hack.png"},
+			{"Sheep Your Hack 3", "Images/Achievements/Sheep_Your_Hack3.png"},
 			{"BulldogJob article", "Images/Achievements/BulldogJob_logo.png"},
 			{"Aquathon", "Images/Achievements/aquathon_logo.png"},
 		};
